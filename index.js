13c58599ff364a6b7e47120bc2cf04c5ebbe9a9b
Jarish3 <giacomo.barbieri@studenti.unitn.it>
2017-12-12 17:11:57 +0100

    Aggiornato index.js [Server] con aggiunta queries

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 //pg config
 var pg = require('pg');
-//Sostituire con link di Heroku vvvvv
+//Sostituire con link di Heroku vvvvv <------------------------------------------------------
 var connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb';
 
 //GET tutte le mense - menù a tendina
@@ -15,7 +15,7 @@ app.get('/mensaALL', function(request, response, next) {
 		
 		console.log("Connesso al database");
 		
-		client.query('SELECT...', function(err, result) {
+		client.query('SELECT Id_mensa, Nome_mensa FROM MENSE', function(err, result) {
 			done();
 			if (err)
 				return console.error('Errore durante esecuzione della query', err);
@@ -33,10 +33,10 @@ app.get('/:id_mensa', function(request, response, next) {
 		
 		console.log("Connesso al database");
 		
-		//Struttura request: params???
-		//Sostituire query vvvvv
-		client.query('SELECT * FROM MENSE WHERE Id_mensa = $1', [request.params.id], function(err, result) {
+		//Struttura request: params??? <---------------------------------------------------
+		client.query('SELECT * FROM MENSE M INNER JOIN ORARI O ON M.Id_mensa=O.Id_mensa AND M.Id_mensa=$1 INNER JOIN FOTO F ON M.Id_mensa=F.Id_mensa AND F.Data = (SELECT MIN(Data) FROM FOTO F WHERE F.Id_mensa=$1)', [request.params.id], function(err, result) {
 			done();
+
 			if (err)
 				return console.error('Errore durante esecuzione della query', err);
 			
@@ -53,19 +53,18 @@ app.get('/:id_gruppo', function(request, response, next) {
 		
 		console.log("Connesso al database");
 		
-		//Struttura request: params???
-		//Sostituire query vvvvv
-		client.query('SELECT * FROM MENSE WHERE Id_gruppo = $1', [request.params.id], function(err, result) {
+		//Struttura request: params??? <---------------------------------------------------
+		client.query('SELECT Id_mensa, Nome_mensa, Tempo_sevizio FROM MENSE WHERE Id_gruppo=$1', [request.params.id], function(err, result) {
 			done();
 			if (err)
 				return console.error('Errore durante esecuzione della query', err);
 			
 			var id_fast = 0;
-			var time = result[0]+.N_persone * result[0].Tempo_servizio;
+			var time = result[0].N_persone * result[0].Tempo_servizio;
 			var temp;
 			 
-			for (int i=1; i<result.size(); i++) {
-				temp = result[i]+.N_persone * result[i].Tempo_servizio;
+			for (var i=1; i<result.size(); i++) {
+				temp = result[i].N_persone * result[i].Tempo_servizio;
 				if (temp < time) {
 					id_fast = i;
 					time = temp;
@@ -76,3 +75,5 @@ app.get('/:id_gruppo', function(request, response, next) {
 		});
 	});
 });
+
+module.exports = app;
