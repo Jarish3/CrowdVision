e908cb19d70b8a3cd6997d498d8e0349b3eef74a
Jarish3 <giacomo.barbieri@studenti.unitn.it>
2017-12-12 18:08:29 +0100

    Aggiornato index.js [Server] con aggiunta queries

--- index.js
+++ index.js
@@ -76,4 +76,35 @@ app.get('/:id_gruppo', function(request, response, next) {
 	});
 });
 
+//GET tempo attesa per info page di singola mensa
+app.get('/tempo_attesa', function(request, response, next) {
+	pg.connect(connectionString, function(err, client, done) {
+		if (err)
+			return console.error('Errore durante il recupero del client dal pool', err);
+		
+		console.log("Connesso al database");
+		
+		//Struttura request: params??? <---------------------------------------------------
+		client.query('SELECT M.Id_mensa, M.Nome_mensa, M.Tempo_sevizio, F.N_persone FROM MENSE M INNER JOIN FOTO F ON M.Id_mensa=F.Id_mensa AND F.Data = (SELECT MIN(Data) FROM FOTO F WHERE F.Id_mensa=$1)', [request.params.id], function(err, result) {
+			done();
+			if (err)
+				return console.error('Errore durante esecuzione della query', err);
+			
+			var id_fast = 0;
+			var time = result[0].N_persone * result[0].Tempo_servizio;
+			var temp;
+			 
+			for (var i=1; i<result.size(); i++) {
+				temp = result[i].N_persone * result[i].Tempo_servizio;
+				if (temp < time) {
+					id_fast = i;
+					time = temp;
+				}
+			}
+			
+			response.send(result[id_fast], time);
+		});
+	});
+});
+
 module.exports = app;
