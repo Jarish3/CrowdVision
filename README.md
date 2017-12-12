1655c95a429ee68a53afa2e8b9673c46e6c9f892
Varen <ychwack@hotmail.it>
2017-12-13 00:37:40 +0100

    -Moved from BuildPack to Dockerfile for Heroku integration (node.js, ASP.Net) -Pooling supports custom rate, repeats -postgreSQL integrated -images retrieved from urls for recognition
    -Completed VisionApi->Pooler
    -Completed Pooler->PostgreSQL
    
    -Heroku attributes for poolingRate, db connection, camera url, computerVision API key

---
+++ README.md
@@ -0,0 +1,2 @@
+# CrowdVision APP
+##Text
---
+++ src/AspNetCoreDemoApp/Program.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreDemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			CrowdVision.App_Start.PodMonitorConfig.Start();
+			new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+
+			CrowdVision.App_Start.PodMonitorConfig.Shutdown();
+
+		}
+	}
+}
