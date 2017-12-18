2c33fac83d02910a52748e5a052cba1e92f91b39
AlessandroG <gnutti.ale@gmail.com>
2017-12-18 18:10:34 +0100

    Insert
    Insert

---
+++ Insert.sql
@@ -0,0 +1,492 @@
+INSERT INTO GRUPPI
+VALUES(
+1,
+"Trento Collina"
+);
+
+INSERT INTO GRUPPI
+VALUES(
+2,
+"Trento Città"
+);
+
+INSERT INTO MENSE
+VALUES ( 
+	1,
+	1,
+	'Povo 0',
+	'via Sommarive, 14',
+	46.06544,
+	11.150565,
+	100,
+	5
+);
+
+INSERT INTO ORARI
+VALUES(
+	1,
+	'Lun',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	1,
+	'Mar',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	1,
+	'Mer',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	1,
+	'Gio',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	1,
+	'Ven',
+	'11.45',
+	'14.30'
+);
+
+
+
+
+
+INSERT INTO MENSE
+VALUES ( 
+	2,
+	1,
+	'Povo 1',
+	'via Sommarive, 5',
+	46.064294,
+	11.150257,
+	100,
+	20
+);
+
+
+INSERT INTO ORARI
+VALUES(
+	2,
+	'Lun',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	2,
+	'Mar',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	2,
+	'Mer',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	2,
+	'Gio',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	2,
+	'Ven',
+	'11.45',
+	'14.30'
+);
+
+
+
+
+
+
+INSERT INTO MENSE
+VALUES ( 
+	3,
+	1,
+	'Mesiano',
+	'via Mesiano, 77',
+	46.061467,
+	11.140377,
+	100,
+	13
+);
+
+
+INSERT INTO ORARI
+VALUES(
+	3,
+	'Lun',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	3,
+	'Mar',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	3,
+	'Mer',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	3,
+	'Gio',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	3,
+	'Ven',
+	'11.45',
+	'14.30'
+);
+
+
+
+
+
+INSERT INTO MENSE
+VALUES ( 
+	4,
+	2,
+	'Tommaso Gar',
+	'via Tomaso Gar, 18',
+	45.465454,
+	9.186516,
+	100,
+	9
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Lun',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Mar',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Mer',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Gio',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Ven',
+	'11.45',
+	'14.30'
+);
+
+
+
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Lun',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Mar',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Mer',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Gio',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	4,
+	'Ven',
+	'18.45',
+	'20.30'
+);
+
+
+
+
+
+INSERT INTO MENSE
+VALUES ( 
+	5,
+	2,
+	'XXIV Maggio',
+	'via XXIV Maggio, 15',
+	46.064402,
+	11.121343,
+	100,
+	17
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Lun',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Mar',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Mer',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Gio',
+	'11.45',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Sab',
+	'12.00',
+	'14.00'
+);
+
+
+
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Lun',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Mar',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Mer',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	5,
+	'Gio',
+	'18.45',
+	'20.30'
+);
+
+INSERT INTO ORARI
+ VALUES(
+	5,
+	'Sab',
+	'10.00',
+	'20.30'
+);
+
+
+
+
+
+
+INSERT INTO MENSE
+VALUES ( 
+	6,
+	2,
+	'UniBar',
+	'via della Malpensada, 138',
+	46.0646752,
+	11.134503,
+	100,
+	15
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Lun',
+	'7.30',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Mar',
+	'7.30',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Mer',
+	'7.30',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Gio',
+	'7.30',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Ven',
+	'7.30',
+	'14.30'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Dom',
+	'12',
+	'14'
+);
+
+
+
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Lun',
+	'17',
+	'21'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Mar',
+	'17.00',
+	'21.00'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Mer',
+	'17.00',
+	'21.00'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Gio',
+	'17.00',
+	'21.00'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Ven',
+	'17.00',
+	'21.00'
+);
+
+INSERT INTO ORARI
+VALUES(
+	6,
+	'Dom',
+	'19.00',
+	'20.30'
+);
